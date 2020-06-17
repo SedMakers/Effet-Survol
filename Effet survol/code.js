c5137e132bdf8f3c95980029a5573a2dc7c48fc2
Sébastien Dijoux <66311255+SedMakers@users.noreply.github.com>
2020-06-17 10:07:42 +0200

    Add files via upload

---
+++ Effet survol/code.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+    var hover = {"index" : 0, "originalText": null, "interval" : null, "node" : null};
+    var nodes = document.querySelectorAll(".sequenceRollover");
+    nodes.forEach(function(item){
+        item.addEventListener("mouseover", function(){
+            
+
+        });
+    });
+});
